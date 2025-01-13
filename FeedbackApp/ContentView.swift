99b7207e362083cb92a4fe3dd518f31bec52930f
casalecchi <felipevilela191@gmail.com>
2025-01-12 22:11:41 -0300

    Initial Commit

---
+++ FeedbackApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FeedbackApp
+//
+//  Created by Felipe Casalecchi on 12/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FeedbackApp/FeedbackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackAppApp.swift
+//  FeedbackApp
+//
+//  Created by Felipe Casalecchi on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
