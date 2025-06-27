2763563c0dee18b6bdc6864a3d4eadbdbdbd573f
casalecchi <felipevilela191@gmail.com>
2025-06-27 14:51:43 -0300

    create Issue View

---
+++ FeedbackApp/IssueView.swift
@@ -0,0 +1,18 @@
+//
+//  IssueView.swift
+//  FeedbackApp
+//
+//  Created by Felipe Casalecchi on 27/06/25.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IssueView()
+}
---
+++ FeedbackApp/NoIssueView.swift
@@ -0,0 +1,18 @@
+//
+//  NoIssueView.swift
+//  FeedbackApp
+//
+//  Created by Felipe Casalecchi on 27/06/25.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
